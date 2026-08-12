6952a09da00847c6d949efee1c08de283aedbdd8
singh-yuvraj1 <yuvraj.singh.95928@gmail.com>
2026-08-12 23:54:44 +0530

    minium changes and added arrow using lucide react

--- frontend1/src/App.jsx
+++ frontend1/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Profile from './components/Profile'
 import ImageCard from './components/ImageCard'
 import "./App.css"
+import { ArrowUpRight } from 'lucide-react';
 
 const App = () => {
   return (
@@ -11,12 +12,12 @@ const App = () => {
         <Profile />
         <Header />            
         
-        <div className = "bg-yellow-300 flex gap-35  border-4 mt-5 h-150 p-10 border-black ">
+        <div className = "relative bg-yellow-300 flex gap-35  border-4 mt-5 h-150 p-10 border-black ">
             <div className = "bg-pink-400 border-2 border-black  ml-35 w-60 h-70 p-3 shrink-0">    
                 <h1 className='text-2xl font-bold '>Prospective Customer Segmentation</h1>
                 <p className='mt-3 text-sm' >Lorem ipsum dolor sit amet consectetur, . Nobis, doloribus voluptates temporibus entium quia voluptate tempora culpa ipsam alias.</p>
             </div>
-                  
+              <div className='absolute left-10 top-100 '><ArrowUpRight size ={140} strokeWidth={1} /></div>
             <div className='flex gap-5 overflow-x-auto scroll-smooth hide-scrollbar' >
               <ImageCard img= "/img2.jpg" about= "Damon Targeryan -Dark sister owner and caraxes rider "/>
               <ImageCard img= "/img1.jpg" about= "Damon Targeryan -Dark sister owner and caraxes rider "/>
